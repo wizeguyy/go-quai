e1c8f93c002a7dba63372c42561787962e657a06
Alejo Acosta <alejoacos@gmail.com>
2024-01-11 12:10:43 -0300

    refactor requestBlockFromPeer() to use protobuf generic functions

--- p2p/node/p2p_services.go
+++ p2p/node/p2p_services.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/dominant-strategies/go-quai/common"
 	"github.com/dominant-strategies/go-quai/core/types"
-	"github.com/dominant-strategies/go-quai/log"
 	"github.com/dominant-strategies/go-quai/p2p/pb"
 	"github.com/dominant-strategies/go-quai/p2p/protocol"
 	"github.com/ipfs/go-cid"
@@ -25,39 +24,34 @@ func (p *P2PNode) requestBlockFromPeer(hash common.Hash, location common.Locatio
 	defer stream.Close()
 
 	// create a block request protobuf message
-	blockReq := pb.CreateProtoBlockRequest(hash, location)
-
-	// Marshal the block request into a byte array
-	blockReqBytes, err := pb.MarshalProtoMessage(blockReq)
+	blockReq, err := pb.CreateProtoBlockRequest(hash, location)
 	if err != nil {
 		return nil, err
 	}
 
 	// Send the block request to the peer
-	err = common.WriteMessageToStream(stream, blockReqBytes)
+	err = common.WriteMessageToStream(stream, blockReq)
 	if err != nil {
 		return nil, err
 	}
 
 	// Read the response from the peer
-	blockResponseBytes, err := common.ReadMessageFromStream(stream)
+	blockResponse, err := common.ReadMessageFromStream(stream)
 	if err != nil {
 		return nil, err
 	}
 
 	// Unmarshal the response into a block
-	var pbBlockResponse pb.BlockResponse
-	err = pb.UnmarshalProtoMessage(blockResponseBytes, &pbBlockResponse)
+	found, pbBlock, err := pb.UnmarshalProtoBlockResponse(blockResponse)
 	if err != nil {
-		log.Errorf("error unmarshalling block response: %s", err)
 		return nil, err
 	}
 
-	// check if the response contains a block
-	if pbBlockResponse.Found {
-		// convert the block to a custom go block type
-		block := pb.ConvertFromProtoBlock(pbBlockResponse.Block)
-		return &block, nil
+	// If the block was found, return it
+	if found {
+		var block *types.Block
+		block.FromProto(pbBlock)
+		return block, nil
 	}
 
 	// If the response does not contain a block, return an error
